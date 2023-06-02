1712c0da028dcbbe0c3f6edd175cf8a970a2caa3
Dominik Deile <dominik.deile@gmail.com>
2023-06-02 15:15:35 +0200

    reformatting and optimizing

--- src/main/java/org/example/ConsoleInteractor.java
+++ src/main/java/org/example/ConsoleInteractor.java
@@ -6,18 +6,16 @@ import java.io.InputStreamReader;
 
 public class ConsolePrinter {
 
-    public static void printToConsole(String filePath, int pageLength) throws IOException {
+    public static void start(String filePath, int pageLength) throws IOException {
 
         var file = new File(FileReader.readFile(filePath), pageLength);
 
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
-        CsvPrinter csvPrinter = new CsvPrinter();
 
         String input = "";
         int currentPage = 1;
-        csvPrinter.printCsv(file, currentPage);
-        printNavigationOutput(currentPage, file.getPages());
+        CsvPrinter.printCsv(file, currentPage);
 
         while (!input.equals("E")) {
             input = br.readLine();
@@ -25,18 +23,18 @@ public class ConsolePrinter {
             switch (input) {
                 case "F" -> currentPage = 1;
                 case "P" -> currentPage = (currentPage <= 1) ? 1 : currentPage - 1;
-                case "N" -> currentPage = (currentPage == file.getPages()) ? file.getPages() : currentPage + 1;
-                case "L" -> currentPage = file.getPages();
+                case "N" -> currentPage = (currentPage == file.getPageCount()) ? file.getPageCount() : currentPage + 1;
+                case "L" -> currentPage = file.getPageCount();
                 case "J" -> {
                     try {
                         var pageInput = Integer.parseInt(br.readLine());
-                        if (!(0 < pageInput && pageInput <= pageLength)) {
+                        if (!(0 < pageInput && pageInput <= file.getPageCount())) {
                             throw new RuntimeException();
                         } else {
                             currentPage = pageInput;
                         }
                     } catch (RuntimeException e) {
-                        System.out.println("Must be a number between 1 and " + file.getPages());
+                        System.out.println("Must be a number between 1 and " + file.getPageCount());
                         System.out.println("Try again:");
                     }
                 }
@@ -45,24 +43,17 @@ public class ConsolePrinter {
                     try {
                         file = FileSorter.sortFile(file, sortInput);
                     } catch (RuntimeException e) {
-                        System.out.println("Must be a valid column");
                         System.out.println("Try again:");
                     }
 
                 }
                 default -> {
                     System.out.println("No valid input");
-                    continue;
                 }
             }
 
-            csvPrinter.printCsv(file, currentPage);
-            printNavigationOutput(currentPage, file.getPages());
+            CsvPrinter.printCsv(file, currentPage);
         }
     }
-
-    private static void printNavigationOutput(int currentPage, int pages) {
-        System.out.println("Page " + currentPage + " of " + pages);
-        System.out.println("F)irst page, P)revious page, N)ext page, L)ast page, J)ump to page, S)ort, E)xit");
-    }
 }
+
